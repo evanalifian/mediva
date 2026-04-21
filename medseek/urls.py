c8fd3602a17ac84cda404b327fa44a505445ad05
evanalifian <evanalifian@gmail.com>
2026-04-21 18:53:23 +0700

    refactor(urls): update URL path structure

--- medseek/urls.py
+++ medseek/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("search/", views.index, name="index"),
+  path("", views.index, name="index"),
+  path("result/", views.result, name="result jawaban"),
 ]
