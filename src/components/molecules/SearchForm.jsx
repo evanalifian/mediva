56a00552618390df3627e5ca3459be2ff81b7614
evanalifian <evanalifian@gmail.com>
2026-05-02 14:47:46 +0700

    feat: implement search form functionality and API data fetching

--- src/components/molecules/SearchForm.jsx
+++ src/components/molecules/SearchForm.jsx
@@ -1,40 +1,37 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function SearchForm() {
   const [query, setQuery] = useState("");
-  const [error, setError] = useState(null);
   const [status, setStatus] = useState("typing");
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setStatus("submitting");
-  }
+  const navigate = useNavigate();
 
   return (
     <>
       <form
-        action="/result"
-        method="GET"
-        autoComplete="off"
-        className="glass p-1.5 rounded-xl md:rounded-2xl shadow-lg shadow-blue-100/40 border border-white flex flex-col md:flex-row items-stretch gap-1.5"
-        onSubmit={handleSubmit}
-      >
-        <div className="flex-1 flex items-center px-3 py-1.5 md:py-0">
-          <i className="fas fa-search text-slate-400 text-sm mr-2.5"></i>
+        method='GET'
+        autoComplete='off'
+        className='glass p-1.5 rounded-xl md:rounded-2xl shadow-lg shadow-blue-100/40 border border-white flex flex-col md:flex-row items-stretch gap-1.5'
+        onSubmit={(e) => {
+          e.preventDefault();
+          setStatus("submitting");
+          navigate(`/result?q=${query}`);
+        }}>
+        <div className='flex-1 flex items-center px-3 py-1.5 md:py-0'>
+          <i className='fas fa-search text-slate-400 text-sm mr-2.5'></i>
           <input
-            type="text"
-            name="query"
-            className="w-full bg-transparent outline-none border-none focus:ring-0 text-xs md:text-sm text-slate-700 font-medium placeholder:text-slate-400"
-            placeholder="Cari gejala, vaksin..."
+            type='text'
+            name='query'
+            className='w-full bg-transparent outline-none border-none focus:ring-0 text-xs md:text-sm text-slate-700 font-medium placeholder:text-slate-400'
+            placeholder='Cari gejala, vaksin...'
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             disabled={status === "submitting"}
           />
         </div>
         <button
-          className="bg-slate-900 text-white px-5 py-2 md:py-2.5 rounded-lg md:rounded-xl text-xs md:text-sm font-bold hover:bg-blue-600 active:scale-95 transition-all shadow-md"
-          disabled={status === "submitting"}
-        >
+          className='bg-slate-900 text-white px-5 py-2 md:py-2.5 rounded-lg md:rounded-xl text-xs md:text-sm font-bold shadow-md transition-all hover:bg-blue-600 active:scale-95 disabled:bg-slate-200 disabled:text-slate-500 disabled:shadow-none disabled:cursor-not-allowed'
+          disabled={query.length === 0 || status === "submitting"}>
           Analyze
         </button>
       </form>
--- src/components/pages/ResultsSearch.jsx
+++ src/components/pages/ResultsSearch.jsx
@@ -1,12 +1,42 @@
 import MainLayout from "../templates/MainLayout";
 import SummaryGroup from "../molecules/SummaryGroup";
 import SearchResults from "../organisms/SearchResults";
+import { useSearchParams } from "react-router";
+import { useEffect, useState } from "react";
 
 export default function ResultsSearch() {
+  const [searchQuery] = useSearchParams();
+  const [results, setResults] = useState();
+  const query = searchQuery.get("q");
+
+  useEffect(() => {
+    let ignore = false;
+    
+    async function handleQuery() {
+      const res = await fetchQuery(query)
+
+      if (!ignore) {
+        setResults(res)
+      }
+    }
+
+    handleQuery()
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <MainLayout>
       <SummaryGroup />
       <SearchResults />
     </MainLayout>
   );
+}
+
+function fetchQuery(query) {
+  return fetch(`http://127.0.0.1:8080/api/search?q=${query}`, {
+    method: "GET",
+  }).then((res) => res.json());
 }
