bb0629f53ff1f128974a5f49b1e7f70725c0ceaa
evanalifian <evanalifian@gmail.com>
2026-05-03 18:01:34 +0700

    chore: update link URL

--- src/components/molecules/TagGroup.jsx
+++ src/components/molecules/TagGroup.jsx
@@ -3,10 +3,10 @@ import TagLink from "../atoms/TagLink";
 export default function TagGroup() {
   return (
     <div className="flex flex-wrap justify-center gap-2 mt-5">
-      <TagLink link="/">Demam berdarah</TagLink>
-      <TagLink link="/">Convid-19</TagLink>
-      <TagLink link="/">Malaria</TagLink>
-      <TagLink link="/">FLu</TagLink>
+      <TagLink link={`/result?q=demam berdarah`}>Demam berdarah</TagLink>
+      <TagLink link={`/result?q=covid-19`}>Convid-19</TagLink>
+      <TagLink link={`/result?q=malaria`}>Malaria</TagLink>
+      <TagLink link={`/result?q=flu`}>Flu</TagLink>
     </div>
   );
 }
